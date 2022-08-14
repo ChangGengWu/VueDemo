446c68935c470c034395830ba594df9806d435e5
AllenWu <a8040333@gmail.com>
2022-08-14 17:33:36 +0800

    first commit

---
+++ src/views/CustomerView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="customer">
+    <h1>This is an customer page</h1>
+  </div>
+  <ul>
+    <li v-for="customer in customers" :key="customer.id">
+        {{customer.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data(){
+    return{
+      customers:[]
+    }
+  },
+  async created(){
+    const res = await axios.get("/customer");
+    console.log(res.data.data);
+    this.customers = res.data.data;
+  }
+}
+</script>
